b98bc0c017e1b653c18c62ee1a2046cd720dc834
KOITT <KOITT@KOITT-PC>
2018-08-23 21:23:36 +0900

    132

--- bdi_mvc/src/com/bdi/mvc/servlet/UserServlet.java
+++ bdi_mvc/src/com/bdi/mvc/servlet/UserServlet.java
@@ -29,20 +29,31 @@ public class UserServlet extends HttpServlet {
 			if(cmd.equals("userList")) {
 				List<Map<String, String>> list = ds.getDelList();
 				request.setAttribute("list",list);
+				
 			}else if(cmd.equals("userView")) {
 				int num = Integer.parseInt(request.getParameter("num"));
 				request.setAttribute("user", ds.getDel(num));
+				
 			}else if(cmd.equals("userInsert")) {
 				String name = request.getParameter("name");
 				String age = request.getParameter("age");
 				request.setAttribute("cnt", ds.insertDel(name, age));
+				
 			}else if(cmd.equals("userUpdate")) {
+				String name = request.getParameter("name");
+				String age = request.getParameter("age");
+				int num = Integer.parseInt(request.getParameter("num"));
+				if(name != null && age != null) {
+					request.setAttribute("cnt", ds.updateDel(num, name, age));
+				}
+				request.setAttribute("user", ds.getDel(num));
 				
 			}else if(cmd.equals("userDelete")) {
 				String[] nums = request.getParameterValues("num");
 				List<Map<String,String>> list = ds.deleteDels(nums);
 				request.setAttribute("list", list);
 				uri = "/views/user/userList";
+				
 			}else {
 				
 			}
